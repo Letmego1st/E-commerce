f935a82c313d7edeb5bbcdcda864f038ad59a369
letmego1st <imcoding4real@gmail.com>
2023-05-09 16:07:21 -0700

    completed models index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,35 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  through: ProductTag, 
+  foreignKey: 'category_id',
+  
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag, 
+  foreignKey: 'product_id',
+ // through: { 
+ //   model: ProductTag,
+ // }
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag, 
+  foreignKey: 'tag_id',
+  // through: {
+  //   model: ProductTag,
+  // }
+  
+});
 
 module.exports = {
   Product,
